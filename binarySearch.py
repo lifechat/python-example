475f03227201cd326ba63323f07bbd88c4a539ab
dengjingjing <dengjingjing@benewtech.cn>
2022-05-24 19:47:55 +0800

    代码commit校验，规范

---
+++ binarySearch.py
@@ -0,0 +1,22 @@
+#  二分法查找
+
+
+# 递归
+def binary_serach():
+    print('递归')
+
+
+# 非递归
+
+
+def binary_serch():
+    print('非递归');
+
+
+
+
+if __name__ == '__main__':
+
+    binary_serach();
+
+    binary_serch();
